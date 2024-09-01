04b916a791ba985afdd83cf108eff325ee71b886
Jakob Schikowski <jakob.schikowski@gmx.de>
2024-09-01 23:21:09 +0200

    Migrate benches to criterion

--- benches/basic_bench.rs
+++ benches/basic_bench.rs
@@ -1,210 +1,223 @@
-#![feature(test)]
-
-extern crate intmap;
-extern crate rand;
-extern crate test;
-
-extern crate indexmap;
-
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use indexmap::IndexMap;
-use intmap::IntMap;
+use intmap::{Entry, IntMap};
 use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use intmap::Entry;
-    use test::Bencher;
+const VEC_COUNT: usize = 10_000;
 
-    const VEC_COUNT: usize = 10_000;
+criterion_group!(
+    benches,
+    u64_insert_built_in,
+    u64_insert_built_in_without_capacity,
+    u64_get_built_in,
+    u64_insert_indexmap,
+    u64_get_indexmap,
+    u64_insert_intmap,
+    u64_insert_intmap_checked,
+    u64_insert_intmap_entry,
+    u64_insert_intmap_without_capacity,
+    u64_resize_intmap,
+    u64_get_intmap,
+);
+criterion_main!(benches);
 
-    // ********** Built in **********
+// ********** Built in **********
 
-    #[bench]
-    fn u64_insert_built_in(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map = HashMap::with_capacity(data.len());
+fn u64_insert_built_in(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map = HashMap::with_capacity(data.len());
 
+    c.bench_function("u64_insert_built_in", |b| {
         b.iter(|| {
             map.clear();
 
             for s in data.iter() {
-                test::black_box(map.insert(s, s));
+                black_box(map.insert(s, s));
             }
-        });
-    }
+        })
+    });
+}
 
-    #[bench]
-    fn u64_insert_built_in_without_capacity(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
+fn u64_insert_built_in_without_capacity(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
 
+    c.bench_function("u64_insert_built_in_without_capacity", |b| {
         b.iter(|| {
             let mut map = HashMap::new();
 
             for s in data.iter() {
-                test::black_box(map.insert(s, s));
+                black_box(map.insert(s, s));
             }
 
-            test::black_box(&map);
-        });
+            black_box(&map);
+        })
+    });
+}
+
+fn u64_get_built_in(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map: HashMap<&u64, &u64> = HashMap::with_capacity(data.len());
+
+    for s in data.iter() {
+        black_box(map.insert(s, s));
     }
 
-    #[bench]
-    fn u64_get_built_in(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map: HashMap<&u64, &u64> = HashMap::with_capacity(data.len());
-
-        for s in data.iter() {
-            test::black_box(map.insert(s, s));
-        }
-
+    c.bench_function("u64_get_built_in", |b| {
         b.iter(|| {
             for s in data.iter() {
-                test::black_box({
+                black_box({
                     map.contains_key(s);
                 });
             }
-        });
-    }
+        })
+    });
+}
 
-    // ********** IndexMap **********
+// ********** IndexMap **********
 
-    #[bench]
-    fn u64_insert_indexmap(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map = IndexMap::with_capacity(data.len());
+fn u64_insert_indexmap(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map = IndexMap::with_capacity(data.len());
 
+    c.bench_function("u64_insert_indexmap", |b| {
         b.iter(|| {
             map.clear();
 
             for s in data.iter() {
-                test::black_box(map.insert(s, s));
+                black_box(map.insert(s, s));
             }
-        });
+        })
+    });
+}
+
+fn u64_get_indexmap(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map: IndexMap<&u64, &u64> = IndexMap::with_capacity(data.len());
+
+    for s in data.iter() {
+        black_box(map.insert(s, s));
     }
 
-    #[bench]
-    fn u64_get_indexmap(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map: IndexMap<&u64, &u64> = IndexMap::with_capacity(data.len());
-
-        for s in data.iter() {
-            test::black_box(map.insert(s, s));
-        }
-
+    c.bench_function("u64_get_indexmap", |b| {
         b.iter(|| {
             for s in data.iter() {
-                test::black_box({
+                black_box({
                     map.contains_key(s);
                 });
             }
-        });
-    }
+        })
+    });
+}
 
-    // ********** Intmap **********
+// ********** Intmap **********
 
-    #[bench]
-    fn u64_insert_intmap(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map = IntMap::with_capacity(data.len());
+fn u64_insert_intmap(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map = IntMap::with_capacity(data.len());
 
+    c.bench_function("u64_insert_intmap", |b| {
         b.iter(|| {
             map.clear();
 
             for s in data.iter() {
-                test::black_box(map.insert(*s, s));
+                black_box(map.insert(*s, s));
             }
-        });
-    }
+        })
+    });
+}
 
-    #[bench]
-    fn u64_insert_intmap_checked(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-        let mut map = IntMap::with_capacity(data.len());
+fn u64_insert_intmap_checked(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+    let mut map = IntMap::with_capacity(data.len());
 
+    c.bench_function("u64_insert_intmap_checked", |b| {
         b.iter(|| {
             map.clear();
 
             for s in data.iter() {
-                test::black_box(map.insert_checked(*s, s));
+                black_box(map.insert_checked(*s, s));
             }
-        });
-    }
+        })
+    });
+}
 
-    #[bench]
-    fn u64_insert_intmap_entry(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
+fn u64_insert_intmap_entry(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
 
-        let mut map = IntMap::with_capacity(data.len());
+    let mut map = IntMap::with_capacity(data.len());
 
+    c.bench_function("u64_insert_intmap_entry", |b| {
         b.iter(|| {
             map.clear();
 
             for s in data.iter() {
-                test::black_box(match map.entry(*s) {
+                black_box(match map.entry(*s) {
                     Entry::Occupied(_) => panic!("unexpected while insert, i = {}", s),
                     Entry::Vacant(entry) => entry.insert(s),
                 });
             }
-        });
-    }
+        })
+    });
+}
 
-    #[bench]
-    fn u64_insert_intmap_without_capacity(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
+fn u64_insert_intmap_without_capacity(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
 
+    c.bench_function("u64_insert_intmap_without_capacity", |b| {
         b.iter(|| {
             let mut map = IntMap::new();
 
             for s in data.iter() {
-                test::black_box(map.insert(*s, s));
+                black_box(map.insert(*s, s));
             }
 
-            test::black_box(&map);
-        });
-    }
+            black_box(&map);
+        })
+    });
+}
 
-    #[bench]
-    fn u64_resize_intmap(b: &mut Bencher) {
+fn u64_resize_intmap(c: &mut Criterion) {
+    c.bench_function("u64_resize_intmap", |b| {
         b.iter(|| {
             let mut map: IntMap<u64> = IntMap::new();
             map.reserve(VEC_COUNT);
-            test::black_box(&map);
-        });
+            black_box(&map);
+        })
+    });
+}
+
+fn u64_get_intmap(c: &mut Criterion) {
+    let data = get_random_range(VEC_COUNT);
+
+    let mut map = IntMap::with_capacity(data.len());
+    for s in data.iter() {
+        map.insert(*s, s);
     }
 
-    #[bench]
-    fn u64_get_intmap(b: &mut Bencher) {
-        let data = get_random_range(VEC_COUNT);
-
-        let mut map = IntMap::with_capacity(data.len());
-        for s in data.iter() {
-            map.insert(*s, s);
-        }
-
+    c.bench_function("u64_get_intmap", |b| {
         b.iter(|| {
             for s in data.iter() {
-                test::black_box(map.contains_key(*s));
+                black_box(map.contains_key(*s));
             }
-        });
+        })
+    });
+}
+
+// ********** Misc **********
+
+fn get_random_range(count: usize) -> Vec<u64> {
+    use rand::prelude::StdRng;
+    use rand::{Rng, SeedableRng};
+
+    let mut vec = Vec::new();
+    let mut rng = StdRng::seed_from_u64(4242);
+
+    for _ in 0..count {
+        vec.push(rng.gen::<u64>());
     }
 
-    // ********** Misc **********
+    vec.sort();
+    vec.dedup();
 
-    fn get_random_range(count: usize) -> Vec<u64> {
-        use rand::prelude::StdRng;
-        use rand::{Rng, SeedableRng};
-
-        let mut vec = Vec::new();
-        let mut rng = StdRng::seed_from_u64(4242);
-
-        for _ in 0..count {
-            vec.push(rng.gen::<u64>());
-        }
-
-        vec.sort();
-        vec.dedup();
-
-        vec
-    }
+    vec
 }
